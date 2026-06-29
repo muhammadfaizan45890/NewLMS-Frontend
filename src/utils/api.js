1d018f95e20da72f666384253d37e501741a296d
Muhammad Faizan <muhammadfaizan45890@gmail.com>
2026-06-29 18:41:51 +0500

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,7 @@
-const API = "https://newlms-backend-production.up.railway.app";
+// const API = "https://newlms-backend-production.up.railway.app";
+// export default API;
+
+
+// src/config/api.js
+const API = process.env.REACT_APP_API_URL;
 export default API;
